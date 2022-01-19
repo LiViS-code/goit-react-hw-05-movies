fc5b66c1526f052139973f0de23aeca3a78b974d
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-19 23:27:57 +0200

    added function

--- my-app/src/utils/ApiService.js
+++ my-app/src/utils/ApiService.js
@@ -7,16 +7,34 @@
 const API_KEY = "b7df999202e1c3618d01db23ce0076f0";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-export function ApiService(query) {
-  return null;
-}
-
-export async function getFilmsTrending() {
+export async function getMovieTrending() {
   try {
     let responce = await fetch(
       `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
     );
     return await responce.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getMovieById(id) {
+  try {
+    let responce = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+    const data = await responce.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getMovieByName(name) {
+  try {
+    let responce = await fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${name}`
+    );
+    const data = await responce.json();
+    return data;
   } catch (error) {
     console.log(error);
   }
