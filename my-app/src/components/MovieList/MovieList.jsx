a1c56eec2fc06f2ebda5b308915a1e1fad2c86c4
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-19 23:26:33 +0200

    added component

---
+++ my-app/src/components/MovieList/MovieList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { ListItem, LinkTo, List } from "../../App.styled";
+
+export default function MovieList({ movieData }) {
+  return (
+    <>
+      <List>
+        {movieData.map(({ id, original_title }) => {
+          return (
+            <ListItem key={id}>
+              <LinkTo to={`/movies/${id}`}>{original_title}</LinkTo>
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
+  );
+}
+
+MovieList.propTypes = {
+  movieData: PropTypes.array.isRequired,
+};
