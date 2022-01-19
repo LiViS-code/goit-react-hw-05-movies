7e16cd149a14f2a3b6c2701092e8416c2e4a1687
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-19 23:26:15 +0200

    added component

---
+++ my-app/src/components/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,70 @@
+import PropTypes from "prop-types";
+import {
+  Container,
+  DataBlock,
+  PosterThumb,
+  Poster,
+  DataContainer,
+  Text,
+  GenresList,
+  GenresListItem,
+  GenreTitle,
+} from "./MovieInfo.styled";
+import { SubTitle, Title } from "../../App.styled";
+
+export default function MovieInfo({ movieData }) {
+  const {
+    original_title,
+    overview,
+    poster_path,
+    release_date,
+    vote_average,
+    genres,
+  } = movieData;
+  return (
+    <>
+      <Container>
+        <PosterThumb>
+          <Poster
+            src={
+              poster_path
+                ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                : "https://image.freepik.com/free-vector/waves-abstract-background-with-coming-soon-text_1017-5060.jpg"
+            }
+            alt="movie poster"
+            width={150}
+          />
+        </PosterThumb>
+
+        <DataContainer>
+          <DataBlock>
+            <Title>
+              {original_title}
+              {release_date ? `(${release_date.slice(0, 4)})` : null}
+            </Title>
+            <Text>User Score: {vote_average}</Text>
+          </DataBlock>
+
+          <DataBlock>
+            <SubTitle>Overview</SubTitle>
+            <Text>{overview}</Text>
+          </DataBlock>
+
+          <DataBlock>
+            <GenreTitle>Genres</GenreTitle>
+            <GenresList>
+              {genres &&
+                genres.map((el) => {
+                  return <GenresListItem key={el.id}>{el.name}</GenresListItem>;
+                })}
+            </GenresList>
+          </DataBlock>
+        </DataContainer>
+      </Container>
+    </>
+  );
+}
+
+MovieInfo.propType = {
+  movieData: PropTypes.array.isRequired,
+};
