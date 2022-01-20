6106076d2ce45a4e3fb951c181bef436733c9927
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-20 21:03:44 +0200

    added route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import NotfoundPage from "./components/NotfoundPage/NotfoundPage";
 import Home from "./routes/Home/Home";
-import Movies from "./routes/Movies/Movies";
-import MovieCard from "./routes/MovieCard/MovieCard";
+import MoviesPage from "./routes/MoviesPage/MoviesPage";
+import MovieDetailsPage from "./routes/MovieDetailsPage/MovieDetailsPage";
 // import MovieList from "./components/MovieList/MovieList";
 
 export default function App() {
@@ -11,8 +11,10 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieCard />} />
+        <Route path="movies" element={<MoviesPage />} />
+        <Route path="movies/:movieId" element={<MovieDetailsPage />} />
+        <Route path="movies/:movieId/cast" element={<MovieDetailsPage />} />
+        <Route path="movies/:movieId/reviews" element={<MovieDetailsPage />} />
         <Route path="*" element={<NotfoundPage />} />
       </Route>
     </Routes>
