6d67e8cfdac9aa443ec8c58ed9e1767907a35f25
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-20 11:34:25 +0200

    refactoring

--- my-app/src/utils/ApiService.js
+++ my-app/src/utils/ApiService.js
@@ -7,35 +7,26 @@
 const API_KEY = "b7df999202e1c3618d01db23ce0076f0";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-export async function getMovieTrending() {
+async function ApiService(URL) {
   try {
-    let responce = await fetch(
-      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
-    );
+    let responce = await fetch(URL);
     return await responce.json();
   } catch (error) {
     console.log(error);
   }
 }
 
+export async function getMovieTrending() {
+  const URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
+  return ApiService(URL);
+}
+
 export async function getMovieById(id) {
-  try {
-    let responce = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
-    const data = await responce.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+  const URL = `${BASE_URL}/movie/${id}?api_key=${API_KEY}`;
+  return ApiService(URL);
 }
 
 export async function getMovieByName(name) {
-  try {
-    let responce = await fetch(
-      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${name}`
-    );
-    const data = await responce.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+  const URL = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${name}`;
+  return ApiService(URL);
 }
