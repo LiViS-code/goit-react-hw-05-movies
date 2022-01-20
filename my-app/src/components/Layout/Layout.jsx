7282ab1c2bb2be5cd36aaf3238be28385469a708
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-20 10:58:17 +0200

    fix visible link

--- my-app/src/components/Layout/Layout.jsx
+++ my-app/src/components/Layout/Layout.jsx
@@ -1,14 +1,25 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import { Header, Footer, Wrapper } from "./Layout.styled";
 
 export default function Layout() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const goBack = () => {
+    navigate(-1);
+  };
+  const isHome = location.pathname === "/" ? true : false;
+
   return (
     <>
       <Header>
         <Navigation />
       </Header>
-      <Link to="/">Go back</Link>
+      {!isHome && (
+        <Link to="/" onClick={goBack}>
+          Go back
+        </Link>
+      )}
       <Wrapper>
         <Outlet />
       </Wrapper>
