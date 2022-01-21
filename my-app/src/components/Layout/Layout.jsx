ac489048c8f9d24805f416be1e20ecb8c180c92e
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-21 22:59:16 +0200

    moved the button back to another location

--- my-app/src/components/Layout/Layout.jsx
+++ my-app/src/components/Layout/Layout.jsx
@@ -1,28 +1,14 @@
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import { Header, Footer, Main } from "./Layout.styled";
-import { LinkTo } from "../../App.styled";
 
 export default function Layout() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const goBack = () => {
-    navigate(-1);
-  };
-  const isHome = location.pathname === "/" ? true : false;
-
   return (
     <>
       <Header>
         <Navigation />
       </Header>
       <Main>
-        {!isHome && (
-          <LinkTo to="" onClick={goBack}>
-            &lArr; Go back
-          </LinkTo>
-        )}
-
         <Outlet />
       </Main>
       <Footer>
