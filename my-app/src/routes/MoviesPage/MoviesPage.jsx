1e9ca5496468080c54d6b0e7ffbe193e5f2b6a5d
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-21 18:44:34 +0200

    upd

--- my-app/src/routes/MoviesPage/MoviesPage.jsx
+++ my-app/src/routes/MoviesPage/MoviesPage.jsx
@@ -19,9 +19,6 @@ export default function MoviesPage() {
     getMovieByName(searchQuery).then((data) => {
       setMovieData(data.results);
     });
-    return () => {
-      // надо очистить фетч...
-    };
   }, [searchQuery]);
 
   return (
