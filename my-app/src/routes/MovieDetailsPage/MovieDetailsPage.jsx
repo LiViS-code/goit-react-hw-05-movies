2842d9c6f8c0986fd05785b5367bd8be5a9386d3
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-21 23:00:09 +0200

    added link "Go back"

--- my-app/src/routes/MovieDetailsPage/MovieDetailsPage.jsx
+++ my-app/src/routes/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,25 +1,38 @@
 import { useState, useEffect } from "react";
+import { Outlet, useParams, useLocation, useNavigate } from "react-router-dom";
 import { LinkTo } from "../../App.styled";
-import { Outlet, useParams } from "react-router-dom";
 import MovieInfo from "../../components/MovieInfo/MovieInfo";
 import { getMovieById } from "../../utils/ApiService";
 
 export default function MovieDetailsPage() {
   const { movieId } = useParams();
   const [movieData, setMovieData] = useState([]);
+  const location = useLocation();
+  const navigation = useNavigate();
+
   useEffect(() => {
     getMovieById(movieId).then((data) => {
       setMovieData(data);
     });
   }, [movieId]);
 
+  const goBack = () => {
+    navigation("/movies");
+    console.log("location MovieDetailsPage :>> ", location);
+  };
+
   return (
     <>
+      <LinkTo to="" onClick={goBack}>
+        &lArr; Go back
+      </LinkTo>
       <MovieInfo movieData={movieData} />
       <p>Additional information</p>
       <ul>
         <li>
-          <LinkTo to="cast">Cast</LinkTo>
+          <LinkTo to="cast" state={{ from: location }}>
+            Cast
+          </LinkTo>
         </li>
         <li>
           <LinkTo to="reviews">Review</LinkTo>
