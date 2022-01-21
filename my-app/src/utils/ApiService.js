f7911bf0228bf7aaa68eda0cf22098df336824e9
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-21 16:50:45 +0200

    del comments

--- my-app/src/utils/ApiService.js
+++ my-app/src/utils/ApiService.js
@@ -1,9 +1,3 @@
-// https://api.themoviedb.org/3/movie/76341?api_key=<<api_key>>
-
-//trending
-// /trending/movie/week
-
-// /search/movie
 const API_KEY = "b7df999202e1c3618d01db23ce0076f0";
 const BASE_URL = "https://api.themoviedb.org/3";
 
