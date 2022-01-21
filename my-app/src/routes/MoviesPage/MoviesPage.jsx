bda00b8b4756b1ada635316f13bbe87152d3d62a
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-21 08:44:29 +0200

    upd

--- my-app/src/routes/MoviesPage/MoviesPage.jsx
+++ my-app/src/routes/MoviesPage/MoviesPage.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import MovieList from "../../components/MovieList/MovieList";
 import { getMovieByName } from "../../utils/ApiService.js";
 
 export default function MoviesPage() {
   const [movieData, setMovieData] = useState(null);
-  // useSt;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const searchQuery = new URLSearchParams(location.search).get("query") ?? "";
 
-  const findMovie = (request) => {
-    getMovieByName(request).then((data) => {
+  let setSearch = (request) => {
+    navigate(`${location.search}?query=${request}`);
+  };
+
+  useEffect(() => {
+    if (!searchQuery) return;
+    getMovieByName(searchQuery).then((data) => {
       setMovieData(data.results);
     });
-  };
+  }, [searchQuery]);
 
   return (
     <>
-      <SearchBar findMovie={findMovie} />
+      <SearchBar setSearch={setSearch} />
       {movieData && <MovieList movieData={movieData} />}
     </>
   );
