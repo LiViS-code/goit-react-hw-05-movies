dfed435bf9be295b4c496f382f0835ab2f4ea3cd
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-22 15:17:10 +0200

    upd

--- my-app/src/routes/MovieDetailsPage/MovieDetailsPage.jsx
+++ my-app/src/routes/MovieDetailsPage/MovieDetailsPage.jsx
@@ -22,12 +22,18 @@ export default function MovieDetailsPage() {
       <p>Additional information</p>
       <ul>
         <li>
-          <LinkTo to="cast" state={{ from: location?.state?.from ?? "/" }}>
+          <LinkTo
+            to={`/movies/${movieId}/cast`}
+            state={{ from: location?.state?.from ?? "/" }}
+          >
             Cast
           </LinkTo>
         </li>
         <li>
-          <LinkTo to="reviews" state={{ from: location?.state?.from ?? "/" }}>
+          <LinkTo
+            to={`/movies/${movieId}/reviews`}
+            state={{ from: location?.state?.from ?? "/" }}
+          >
             Review
           </LinkTo>
         </li>
