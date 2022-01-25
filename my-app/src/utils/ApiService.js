f638fe7147e40e7b12b4362c36d310b29f57af78
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-25 22:53:52 +0200

    upd api-key

--- my-app/src/utils/ApiService.js
+++ my-app/src/utils/ApiService.js
@@ -1,4 +1,4 @@
-const API_KEY = "b7df999202e1c3618d01db23ce0076f0";
+const API_KEY = "?api_key=b7df999202e1c3618d01db23ce0076f0";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 async function ApiService(URL) {
@@ -11,26 +11,26 @@ async function ApiService(URL) {
 }
 
 export async function getMovieTrending() {
-  const URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
+  const URL = `${BASE_URL}/trending/movie/day${API_KEY}`;
   return ApiService(URL);
 }
 
 export async function getMovieById(id) {
-  const URL = `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`;
+  const URL = `${BASE_URL}/movie/${id}${API_KEY}&language=en-US`;
   return ApiService(URL);
 }
 
 export async function getMovieByName(name) {
-  const URL = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${name}&language=en-US&page=1`;
+  const URL = `${BASE_URL}/search/movie${API_KEY}&query=${name}&language=en-US&page=1`;
   return ApiService(URL);
 }
 
 export async function getCast(id) {
-  const URL = `${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`;
+  const URL = `${BASE_URL}/movie/${id}/credits${API_KEY}&language=en-US`;
   return ApiService(URL);
 }
 
 export async function getReviews(id) {
-  const URL = `${BASE_URL}/movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
+  const URL = `${BASE_URL}/movie/${id}/reviews${API_KEY}&language=en-US&page=1`;
   return ApiService(URL);
 }
